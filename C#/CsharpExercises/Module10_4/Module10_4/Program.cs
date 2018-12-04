8932b71ba66090a82387ec498e7687a61156eb9d
RIProG <riprog@hotmail.com>
2018-12-04 14:18:50 +0100

    Lagt in övningar från Module 9 & 10

---
+++ C#/CsharpExercises/Module10_4/Module10_4/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Module10_4
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<string> nameList = new List<string>();
+            int nameCounter = 0;
+            bool loop = true;
+            bool validName;
+
+            while (loop == true)
+            {
+                string inputName = AskForName();
+                (validName, loop) = ValidateName(inputName);
+                if (validName == true)
+                {
+                    nameCounter++;
+                    nameList.Add(inputName);
+                }
+
+            }
+            nameList.Sort();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            if (nameCounter > 2)
+            {
+                nameList.RemoveAt(nameCounter-1);
+                nameList.RemoveAt(0);
+            }
+
+            foreach (string name in nameList)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+
+        private static string AskForName()
+        {
+            Console.Write("Enter a name: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            string inputName = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            return inputName;
+        }
+        private static (bool, bool) ValidateName(string inputName)
+        {
+            bool validName;
+            bool loop;
+            if (inputName.ToLower() == "quit")
+            {
+                validName = false;
+                loop = false;
+                return (validName, loop);
+            }
+            else if (inputName.ToLower() != "quit")
+            {
+                foreach (char letter in inputName)
+                {
+                    if (!char.IsLetter(letter))
+                    {
+                        validName = false;
+                        loop = true;
+                        return (validName, loop);
+                    }
+                }
+            }
+            else
+            {
+                validName = true;
+                loop = true;
+                return (validName, loop);
+            }
+            return (true, true);
+
+        }
+    }
+}
---
+++ C#/CsharpExercises/Module9_2/Module9_2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace Module9_2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine(@"I'm watching this folder C:\Temp");
+
+            var x = new FileSystemWatcher();
+            x.Path = @"C:\Temp";
+            x.EnableRaisingEvents = true;
+            x.Created += AFileIsCreated;
+            x.Deleted += AFileIsDeleted;
+            x.Changed += AFileIsChanged;
+            x.Renamed += AFileIsRenamed;
+            Console.ReadKey();
+
+
+        }
+
+        private static void AFileIsCreated(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"{e.Name} has been created!");
+        }
+
+        private static void AFileIsChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"{e.Name} has been changed!");
+            //if (e.Name == "onsdag.txt")
+            //{
+            //    (@"C:\Temp\statistics.txt", e);
+            //}
+        }
+
+        private static void AFileIsDeleted(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"{e.Name} has been deleted!");
+        }
+
+        private static void AFileIsRenamed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"{e.Name} has been renamed!");
+        }
+    }
+}
