cc0cc7f38a04533ed1841a09c2e272dded2e7901
RIProG <riprog@hotmail.com>
2018-12-07 13:48:29 +0100

    Uppdaterat methods and lists-övningar

---
+++ C#/CsharpExercises/MethodsAndLists/MethodsAndLists.Core/StringToBool.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using MethodsAndLists.Core.Models;
+
+namespace MethodsAndLists.Core
+{
+    public class StringToBool
+    {
+        public bool IsPalindrome(string word)
+        {
+            if (string.IsNullOrWhiteSpace(word))
+            {
+                return false;
+            }
+
+            char[] charArray = word.ToCharArray();
+            Array.Reverse(charArray);
+            string newWord = new string(charArray);
+
+            if (newWord.ToLower() == word.ToLower())
+                return true;
+            else
+                return false;
+
+        }
+
+        public bool IsZipCode(string input)
+        {
+            return Regex.IsMatch(input, @"^[1-9]\d\d\s\d\d$");
+        }
+
+        public bool IsFiveLettersOrLonger(string word)
+        {
+            if (word.Length < 5)
+            {
+                return false;
+            }
+            else
+                return true;
+        }
+    }
+
+}
---
+++ C#/CsharpExercises/MethodsAndLists/MethodsAndLists.Test/StringToBoolTests.cs
@@ -0,0 +1,66 @@
+﻿using MethodsAndLists.Core;
+using MethodsAndLists.Core.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MethodsAndLists.Test
+{
+    [TestClass]
+    public class StringToBoolTests
+    {
+        StringToBool x = new StringToBool();
+
+        [TestMethod]  
+        public void palindrome_should_return_palindrome()
+        {
+            Assert.IsFalse(x.IsPalindrome(null));
+            Assert.IsTrue(x.IsPalindrome("AnnA"));
+            Assert.IsFalse(x.IsPalindrome("Greger"));
+            Assert.IsTrue(x.IsPalindrome("12321"));
+            Assert.IsTrue(x.IsPalindrome("Kalasalak"));
+            Assert.IsFalse(x.IsPalindrome("Mamma"));
+            Assert.IsTrue(x.IsPalindrome("Alla"));
+        }
+
+        [TestMethod]
+        
+        public void isZipCode_should_return_if_valid_zipcode_or_not()
+        {
+
+            Assert.IsTrue(x.IsZipCode("234 54"));
+            Assert.IsFalse(x.IsZipCode("03245"));
+            Assert.IsFalse(x.IsZipCode("PANDA"));
+            Assert.IsFalse(x.IsZipCode("5 3 2"));
+            Assert.IsTrue(x.IsZipCode("444 42"));
+        }
+
+        [TestMethod]
+        [DataRow ("Banan")]
+        [DataRow ("Roddbåt")]
+        [DataRow ("Trollkarlar")]
+        [DataRow ("Filhanteraren")]
+        [DataRow ("Ögrupp")]
+
+        public void if_all_words_are_five_letters_or_longer_should_return_true(string word)
+        {
+
+            Assert.IsTrue(x.IsFiveLettersOrLonger(word));
+        }
+
+        [TestMethod]
+        [DataRow("Bana")]
+        [DataRow("Rep")]
+        [DataRow("Lo")]
+        [DataRow("Ö")]
+        [DataRow("Man")]
+
+        public void if_all_words_are_five_letters_or_longer_should_return_false(string word)
+        {
+
+            Assert.IsFalse(x.IsFiveLettersOrLonger(word));
+        }
+
+    }
+}
